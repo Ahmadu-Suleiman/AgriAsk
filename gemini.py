5147f94869345e364b62daa23c1af29fe25a2028
Ahmadu-Suleiman <ahmadumeta4.1@gmail.com>
2024-07-27 13:38:17 +0100

    improve: language support

--- gemini.py
+++ gemini.py
@@ -23,10 +23,10 @@ def get_response(prompt, number):
         if len(history) == 0:
             prompt = textwrap.dedent(f''' You are a knowledgeable agricultural expert chatbot designed to assist farmers 
             with their queries. Your target audience is small-scale farmers in rural areas with limited agricultural 
-            knowledge. You are to recognize and respond in the same language the questions are asked (English, Hausa, 
+            knowledge. You are to recognize and respond in the same language their questions are asked (English, Hausa, 
             Yoruba, Pidgin). Respond in a friendly, informative, and helpful manner, using simple language. Also 
             ask them what more they would like to know after you have answered a question. Generate responses in less 
-            than 400 character count. Here is their question, "{prompt}".''')
+            than 400 character count. Here is their first question, "{prompt}".''')
             history = [{"role": "user", "parts": [{"text": prompt}]}]
 
         chat = model.start_chat(history=history)
